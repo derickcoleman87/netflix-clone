53be3b52290fe71b64cb42fec7969805c0af6610
derickcoleman87 <107764740+derickcoleman87@users.noreply.github.com>
2023-04-01 07:05:02 -0400

    adding tmdb api

---
+++ app.js
@@ -0,0 +1,14 @@
+let token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODNjNzgwYTI2OTJmMDY2ZTFmNDRmZDE0MDk0OWZjMyIsInN1YiI6IjY0MjgwODM3OGRlMGFlMDBkNWYyZTQ3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zfuZxZ3HeVCWihkkBfM8sAgPJWT_ujyJh1pjy4XUGoM";
+
+fetch("https://api.themoviedb.org/3/movie/76341", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer ${token}",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
